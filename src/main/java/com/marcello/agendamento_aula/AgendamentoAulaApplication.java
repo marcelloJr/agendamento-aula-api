47b3e8242bd2d1cb2d568640819e4d67df3698c6
Marcello Henrique <mhmj2425@gmail.com>
2022-02-01 18:06:49 -0300

    Criação do projeto Spring Boot

---
+++ src/main/java/com/marcello/agendamento_aula/AgendamentoAulaApplication.java
@@ -0,0 +1,13 @@
+package com.marcello.agendamento_aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcello/agendamento_aula/AgendamentoAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcello.agendamento_aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
